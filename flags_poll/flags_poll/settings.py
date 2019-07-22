0d2ebd3b398c158fb5130eb98ad003b6c82acf2c
Dmytro Tvoretc <rus.tvoretc@gmail.com>
2019-07-22 14:54:13 +0300

    changed settings db connection

--- flags_poll/flags_poll/settings.py
+++ flags_poll/flags_poll/settings.py
@@ -80,12 +80,25 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Flags',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'USER': 'Tvoretc',
+        'PASSWORD': 'rootroot',
+        'HOST': 'Tvoretc.mysql.pythonanywhere-services.com',
         'PORT': '',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Flags',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
