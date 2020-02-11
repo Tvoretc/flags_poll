55d6cab6035b988b403ae772ef7bf7c16f5003ed
Dmytro Tvoretc <rus.tvoretc@gmail.com>
2020-02-11 20:32:06 +0200

    print password added

--- flags_poll/settings.py
+++ flags_poll/settings.py
@@ -155,10 +155,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'fereftbot@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('PYTHONANYWHERE')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-if EMAIL_HOST_PASSWORD == None:
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+# if os.getenv('PYTHONANYWHERE'):
+#     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
